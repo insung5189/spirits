179b07857663ca98604eb35d33bf65f21ff5a4c5
insung5189 <insung5189@gmail.com>
2023-08-25 23:49:25 +0900

    이메일

--- src/main/java/com/ll/spirits/email/MailController.java
+++ src/main/java/com/ll/spirits/email/MailController.java
@@ -35,7 +35,6 @@ public class MailController {
         String title = "안녕하세요 주류추천 서비스 Spirits 입니다. 회원가입시 필요한 인증번호 입니다.";
         String content = "[인증번호] " + mailKey + " 입니다. <br/> 인증번호 확인란에 기입해주십시오.<br/> 제작자의 다른 포트폴리오 : "+ url;
 
-        content += url;
         try {
             MimeMessage mail = mailSender.createMimeMessage();
             MimeMessageHelper mailHelper = new MimeMessageHelper(mail, true, "UTF-8");
